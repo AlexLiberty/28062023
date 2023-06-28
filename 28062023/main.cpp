0cd63d25289897f453432809eeef0db6c348e82c
Alex Yelienok <aelenok@gmail.com>
2023-06-28 20:41:12 +0300

    додана реалізація через вказівник

--- 28062023/main.cpp
+++ 28062023/main.cpp
@@ -12,15 +12,20 @@ int main()
     SetConsoleCP(1251);
     SetConsoleOutputCP(1251);
 
-    Dog dog("Рекс","Собака", 3);
-    Cat cat("Мурка", 4);
-    Parrot parrot("Кеша", 100);
+    /*Dog dog("Рекс","Собака", 3);*/
+    //Cat cat("Мурка", 4);
+    //Parrot parrot("Кеша", 100);
 
-    dog.GetCharacteristics();
-    dog.Sound();
+    //dog.GetCharacteristics();
+    //dog.Sound();
     //cat.GetCharacteristics();
     //parrot.GetCharacteristics();
 
+    Animals* dog = new Dog("Бобік", "собака", 3);
+    dog->GetCharacteristics();
+    dog->Sound();
+
+    delete dog;
 
 
     return 0;
